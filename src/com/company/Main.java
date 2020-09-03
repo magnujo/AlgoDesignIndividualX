71aaeef74097a6ab02e4822eca9ed64f1dabc09b
Magnus <magnus_johannsen@hotmail.com>
2020-09-03 11:18:10 +0200

    Parser done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Parser parser = new Parser();
+
+        ArrayList<Pair<Integer, Integer>> set = parser.Scan();
+        parser.Print();
+
+
+
+
+
     }
 }
+
+
+class Parser {
+    ArrayList<Pair<Integer, Integer>> set = new ArrayList<Pair<Integer, Integer>>();
+    Scanner sc = new Scanner(System.in);
+    Boolean firstRun = true;
+    public ArrayList<Pair<Integer, Integer>> Scan (){
+        int number = sc.nextInt();
+        System.out.println(number);
+        for (int i = 0; i < number; i++) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
+            Pair<Integer, Integer> pair = new Pair<Integer, Integer>(first,second);
+            if(firstRun==false && set.get(0).getValue()>=pair.getValue()){
+                set.add(pair);
+            }
+            else for (int j = 0; j < set.size(); j++) {
+                if (set.get(j).getValue()>=pair.getValue())
+                    set.add(j, pair);
+
+            }
+
+            firstRun=false;
+        }
+        return set;
+    }
+
+    public Boolean Print(){
+        for (int i = 0; i < set.size(); i++) {
+            System.out.println(set.get(i));
+        }
+        return true;
+    }
+
+}
+
